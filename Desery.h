6b6e696a2baa89014b9e4ed3c3197095f15919a5
lamoninhoxd <86768947+lamoninhoxd@users.noreply.github.com>
2022-05-17 08:46:44 +0200

    Uzupelniony Main o lody
    Uzupelnilem i stworzylem 3 rodzaje lodow, przetestowane w main

--- Desery.h
+++ Desery.h
@@ -8,12 +8,13 @@ class Desery{
         string nazwa;
         double cena;
         vector<string> Wielkosc;
+        vector<double> Wielkosc_Cena;
         vector<string> Rodzaje;
         vector<double> Rodzaje_Cena;
         vector<string> Skladniki;
         vector<double> Skladniki_Cena;
-        vector<string> Sosy;
-        vector<double> Sosy_Cena;
+        vector<string> Dodatki;
+        vector<double> Dodatki_Cena;
     public:
         Desery(string nazwa, double cena){
             this->nazwa = nazwa;
@@ -23,9 +24,11 @@ class Desery{
             this->nazwa = "default";
             this->cena = 0.00;
         }
+
     //Funkcje dodające atrybuty klasy do vectorów
-        void DodajWielkosc(string wielkosc){
+        void DodajWielkosc(string wielkosc, double cena){
             Wielkosc.push_back(wielkosc);
+            Wielkosc_Cena.push_back(cena);
          }
         void DodajRodzaj(string nazwa, double cena){
             Rodzaje.push_back(nazwa);
@@ -35,45 +38,59 @@ class Desery{
             Skladniki.push_back(nazwa);
             Skladniki_Cena.push_back(cena);
         }
-        void DodajSos(string nazwa, double cena){
-            Sosy.push_back(nazwa);
-            Sosy_Cena.push_back(cena);
+        void DodajDodatki(string nazwa, double cena){
+            Dodatki.push_back(nazwa);
+            Dodatki_Cena.push_back(cena);
         }
+
     //Gettery i Settery
         void SetNazwa(string nazwa){ this->nazwa = nazwa;  }
         void SetCena(double cena){ this->cena = cena; }
         string GetNazwa(){return nazwa;}
         double GetCena(){return cena;}
         string GetWielkosc( int indeks ){ return Wielkosc[indeks]; }
+        double GetWielkoscCena( int indeks ){ return Wielkosc_Cena[indeks]; }
         string GetRodzaj(int indeks ){ return Rodzaje[indeks]; }
         double GetRodzajCena(int indeks ){ return Rodzaje_Cena[indeks]; }
         string GetSkladnik(int indeks ){ return Skladniki[indeks]; }
         double GetSkladnikCena( int indeks ){ return Skladniki_Cena[indeks]; }
-        string GetSos( int indeks ){ return Sosy[indeks]; }
-        double GetSosCena( int indeks ){ return Sosy_Cena[indeks]; }
+        string GetDodatki( int indeks ){ return Dodatki[indeks]; }
+        double GetDodatkiCena( int indeks ){ return Dodatki_Cena[indeks]; }
+
     //Funkcja wypisująca zawartość obiektu klasy - głównie do testów
         void Wypisz(){ 
             cout<<GetCena()<<endl<<GetNazwa()<<endl; 
-            int i=0;
-            while(i!=Wielkosc[i].size()){
-                cout<<Wielkosc[i]<<endl;
-                i++;
-            }   
-            i=0; 
-            while(i!=Rodzaje[i].size()){
-                cout<<Rodzaje[i]<<"     "<<Rodzaje_Cena[i]<<endl;
-                i++;
-            }
-            i=0;
-            while(i!=Skladniki[i].size()){
-                cout<<Skladniki[i]<<"   "<<Skladniki_Cena[i]<<endl;
-                i++;
-            }
-            i=0;
-            while(i!=Sosy[i].size()){
-                cout<<Sosy[i]<<"    "<<Sosy_Cena[i]<<endl;
-                i++;
-            }
+            WypiszWielkosci();
+            WypiszRodzaje();
+            WypiszSkladniki();
+            WypiszDodatki();
         }
 
+        void WypiszWielkosci(){
+            cout<<"-----Rozmiary-----"<<endl;
+            for(int i=0; i<Wielkosc.size(); i++){
+                cout<<i+1<<": "<<Wielkosc[i]<<endl<<"      "<<Wielkosc_Cena[i]<<endl;
+            } 
+        }
+        void WypiszRodzaje(){  
+            cout<<"-----Rodzaje-----"<<endl;  
+            for(int i=0; i<Rodzaje.size(); i++){
+                cout<<i+1<<": "<<Rodzaje[i]<<"     "<<Rodzaje_Cena[i]<<endl;
+            }
+        }
+        void WypiszSkladniki(){
+            cout<<"-----Skladniki-----"<<endl;
+            for(int i=0; i<Skladniki.size(); i++){
+                cout<<i+1<<": "<<Skladniki[i]<<"     "<<Skladniki_Cena[i]<<endl;
+            }
+        }
+        void WypiszDodatki(){
+            cout<<"-----Dodatki-----"<<endl;
+            for(int i=0; i<Dodatki.size(); i++){
+                cout<<i+1<<": "<<Dodatki[i]<<"    "<<Dodatki_Cena[i]<<endl;
+            }
+        }
 };
+
+
+
--- main.cpp
+++ main.cpp
@@ -5,13 +5,159 @@
 
 using namespace std;
 
+Desery LodywKubeczku("Lody w kubeczku", 2.99);
+Desery LodywWafelku("Lodu w wafelku", 2.50);
+Desery LodywRozku("Rożek", 1.50);
+Desery Shake("Shake", 4.99);
+Paragon p1;
+
+
+void DeklaracjaLodywKubku(){
+    LodywKubeczku.DodajWielkosc("Male", 0);
+	LodywKubeczku.DodajWielkosc("Srednie", 1);
+	LodywKubeczku.DodajWielkosc("Duze", 1.5);
+	LodywKubeczku.DodajRodzaj("Vegan", 1);
+	LodywKubeczku.DodajRodzaj("Bez cukru", 0.5);
+	LodywKubeczku.DodajRodzaj("Bez laktozy", 1.5);
+	LodywKubeczku.DodajSkladniki("Oreo", 1);
+	LodywKubeczku.DodajSkladniki("Posypka lion", 1);
+	LodywKubeczku.DodajDodatki("Czekoladowy", 0);
+	LodywKubeczku.DodajDodatki("Karmelowy", 0);
+}
+
+void DeklaracjaLodywWaflu(){
+	LodywWafelku.DodajWielkosc("Male", 0);
+	LodywWafelku.DodajWielkosc("Srednie", 1);
+	LodywWafelku.DodajWielkosc("Duze", 1.5);
+	LodywWafelku.DodajRodzaj("Vegan", 1);
+	LodywWafelku.DodajRodzaj("Bez cukru", 0.5);
+	LodywWafelku.DodajRodzaj("Bez laktozy", 1.5);
+	LodywWafelku.DodajSkladniki("Oreo", 1);
+	LodywWafelku.DodajSkladniki("Posypka lion", 1);
+	LodywWafelku.DodajDodatki("Czekoladowy", 0);
+	LodywWafelku.DodajDodatki("Karmelowy", 0);
+}
+
+void DeklaracjaRozek(){
+	LodywRozku.DodajWielkosc("Male", 0);
+	LodywRozku.DodajWielkosc("Srednie", 1);
+	LodywRozku.DodajWielkosc("Duze", 1.5);
+	LodywRozku.DodajRodzaj("Vegan", 1);
+	LodywRozku.DodajRodzaj("Bez cukru", 0.5);
+	LodywRozku.DodajRodzaj("Bez laktozy", 1.5);
+	LodywRozku.DodajSkladniki("Oreo", 1);
+	LodywRozku.DodajSkladniki("Posypka lion", 1);
+	LodywRozku.DodajDodatki("Czekoladowy", 0);
+	LodywRozku.DodajDodatki("Karmelowy", 0);
+}
+
+void DeklaracjaShake(){
+	Shake.DodajWielkosc("Maly", 0);
+	Shake.DodajWielkosc("Sredni", 1);
+	Shake.DodajWielkosc("Duzy", 1.5);
+
+
+}
+
 int main(){
 	srand(time(NULL));
-	Desery t1("Lody", 10.69);
-	Paragon p1;
-	p1.DodajDoParagonu(t1.GetNazwa(), t1.GetCena());
-	cin.get();
+	DeklaracjaLodywKubku();
+	DeklaracjaLodywWaflu();
+	DeklaracjaRozek();
+	LodywKubeczku.Wypisz();
+
+	int wybor;
+
+	while(1){
+		int skladniki=0;
+
+	cout<<"==================Kawiarnia=================="<<endl;
+	cout<<"Proszę wybrać interesujący Państwa deser: "<<endl;
+	cout<<"1. Lody"<<endl;
+	cout<<"2. Shake"<<endl;
+	cout<<"3. Kawa Mrożona"<<endl;
+	cin>>wybor;
+	switch(wybor){
+		case 1:
+			cout<<"1. "<<LodywKubeczku.GetNazwa()<<"	"<<LodywKubeczku.GetCena()<<endl;
+			cout<<"2. "<<LodywRozku.GetNazwa()<<"	"<<LodywKubeczku.GetCena()<<endl;
+			cout<<"3. "<<LodywWafelku.GetNazwa()<<"		"<<LodywWafelku.GetCena()<<endl;
+			cin>>wybor;
+			switch(wybor){
+				case 1:
+					p1.DodajDoParagonu(LodywKubeczku.GetNazwa(), LodywKubeczku.GetCena());
+					LodywKubeczku.WypiszWielkosci();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywKubeczku.GetWielkosc(wybor-1), LodywKubeczku.GetWielkoscCena(wybor-1));
+					LodywKubeczku.WypiszRodzaje();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywKubeczku.GetRodzaj(wybor-1), LodywKubeczku.GetRodzajCena(wybor-1));
+					cout<<"Do wybranego rodzaju lodów można dobrać 2 składniki"<<endl;
+					cout<<"Wybierz '0' dla pominięcia wyboru">>endl;
+					LodywKubeczku.WypiszSkladniki();
+					do{
+					cin>>wybor;
+					if(wybor==0) break;
+					p1.DodajDoParagonu(LodywKubeczku.GetSkladnik(wybor-1), LodywKubeczku.GetSkladnikCena(wybor-1));
+					skladniki++;
+					}while(skladniki!=2);
+					LodywKubeczku.WypiszDodatki();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywKubeczku.GetDodatki(wybor-1), LodywKubeczku.GetDodatkiCena(wybor-1));
+					break;
+
+				case 2:
+					p1.DodajDoParagonu(LodywRozku.GetNazwa(), LodywRozku.GetCena());
+					LodywRozku.WypiszWielkosci();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywRozku.GetWielkosc(wybor-1), LodywRozku.GetWielkoscCena(wybor-1));
+					LodywRozku.WypiszRodzaje();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywRozku.GetRodzaj(wybor-1), LodywRozku.GetRodzajCena(wybor-1));
+					cout<<"Do wybranego rodzaju lodów można dobrać 2 składniki"<<endl;
+					cout<<"Wybierz '0' dla pominięcia wyboru">>endl;
+					LodywRozku.WypiszSkladniki();
+					do{
+					cin>>wybor;
+					if(wybor==0) break;
+					p1.DodajDoParagonu(LodywRozku.GetSkladnik(wybor-1), LodywRozku.GetSkladnikCena(wybor-1));
+					skladniki++;
+					}while(skladniki!=2);
+					LodywRozku.WypiszDodatki();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywRozku.GetDodatki(wybor-1), LodywRozku.GetDodatkiCena(wybor-1));
+					break;
+
+				case 3:
+					p1.DodajDoParagonu(LodywWafelku.GetNazwa(), LodywWafelku.GetCena());
+					LodywWafelku.WypiszWielkosci();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywWafelku.GetWielkosc(wybor-1), LodywWafelku.GetWielkoscCena(wybor-1));
+					LodywWafelku.WypiszRodzaje();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywWafelku.GetRodzaj(wybor-1), LodywWafelku.GetRodzajCena(wybor-1));
+					cout<<"Do wybranego rodzaju lodów można dobrać 2 składniki"<<endl;
+					cout<<"Wybierz '0' dla pominięcia wyboru">>endl;
+					LodywWafelku.WypiszSkladniki();
+					do{
+					cin>>wybor;
+					if(wybor==0) break;
+					p1.DodajDoParagonu(LodywWafelku.GetSkladnik(wybor-1), LodywWafelku.GetSkladnikCena(wybor-1));
+					skladniki++;
+					}while(skladniki!=2);
+					LodywWafelku.WypiszDodatki();
+					cin>>wybor;
+					p1.DodajDoParagonu(LodywWafelku.GetDodatki(wybor-1), LodywWafelku.GetDodatkiCena(wybor-1));
+					break;
+			}
+		
+		
+		break;
+	}
 	p1.PodajParagon();
+	}
+	
+	
 	
     return 0;
 }
